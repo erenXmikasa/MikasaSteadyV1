02ac698289cb8a10fba04eddb2e84e972476f8b3
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2021-12-28 04:15:30 +0700

    [STEADY] Update Speedtest

--- Music/Plugins/speedtest.py
+++ Music/Plugins/speedtest.py
@@ -15,7 +15,7 @@ async def gstats(_, message):
             pass
         else:
             return
-    m = await message.reply_text("__Running VieenaSpeedTest From Server__")
+    m = await message.reply_text("__Running SpeedTest From Server__")
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
