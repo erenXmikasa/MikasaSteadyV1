03030ddc76db9e891cdd237aec5053e20cd23659
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-01-18 00:33:51 +0700

    Update stream.py

--- Music/Plugins/stream.py
+++ Music/Plugins/stream.py
@@ -320,25 +320,25 @@ async def vplay(c: Client, message: Message):
 💬 **Diputar di:** {message.chat.title}
 """
 
- @app.on_message(command("vplaylist") & filters.group)
- async def playlist(client, m: Message):
-     chat_id = m.chat.id
-     if chat_id in QUEUE:
-         chat_queue = get_queue(chat_id)
-         if len(chat_queue) == 1:
-             await m.delete()
-             await m.reply(
-                 f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
-                 disable_web_page_preview=True,
-             )
-         else:
-             QUE = f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ DAFTAR ANTRIAN:**"
-             l = len(chat_queue)
-             for x in range(1, l):
-                 hmm = chat_queue[x][0]
-                 hmmm = chat_queue[x][2]
-                 hmmmm = chat_queue[x][3]
-                 QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`\n"
-             await m.reply(QUE, disable_web_page_preview=True)
-     else:
-         await m.reply("**❌ Tidak memutar apapun**")
+@app.on_message(command("playlist") & filters.group)
+async def playlist(client, m: Message):
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        chat_queue = get_queue(chat_id)
+        if len(chat_queue) == 1:
+            await m.delete()
+            await m.reply(
+                f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
+                disable_web_page_preview=True,
+            )
+        else:
+            QUE = f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ DAFTAR ANTRIAN:**"
+            l = len(chat_queue)
+            for x in range(1, l):
+                hmm = chat_queue[x][0]
+                hmmm = chat_queue[x][2]
+                hmmmm = chat_queue[x][3]
+                QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`\n"
+            await m.reply(QUE, disable_web_page_preview=True)
+    else:
+        await m.reply("**❌ Tidak memutar apapun**")
