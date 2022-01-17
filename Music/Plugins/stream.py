68bab29f27fcd77d5287288d5aaaad74832e79cb
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-01-18 00:37:19 +0700

    Update stream.py

--- Music/Plugins/stream.py
+++ Music/Plugins/stream.py
@@ -319,32 +319,6 @@ async def vplay(c: Client, message: Message):
 🎧 **Atas permintaan:** {requester}
 💬 **Diputar di:** {message.chat.title}
 """
-
-@app.on_message(command("playlist") & filters.group)
-async def playlist(client, m: Message):
-    chat_id = m.chat.id
-    if chat_id in QUEUE:
-        chat_queue = get_queue(chat_id)
-        if len(chat_queue) == 1:
-            await m.delete()
-            await m.reply(
-                f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
-                disable_web_page_preview=True,
-            )
-        else:
-            QUE = f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ DAFTAR ANTRIAN:**"
-            l = len(chat_queue)
-            for x in range(1, l):
-                hmm = chat_queue[x][0]
-                hmmm = chat_queue[x][2]
-                hmmmm = chat_queue[x][3]
-                QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`\n"
-            await m.reply(QUE, disable_web_page_preview=True)
-    else:
-        await m.reply("**❌ Tidak memutar apapun**")
-
-#p
-
 @Client.on_callback_query(filters.regex(pattern=r"popat"))
 async def popat(_,CallbackQuery): 
     callback_data = CallbackQuery.data.strip()
@@ -411,3 +385,26 @@ async def popat(_,CallbackQuery):
         )  
         return
             
+@app.on_message(command("playlist") & filters.group)
+async def playlist(client, m: Message):
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        chat_queue = get_queue(chat_id)
+        if len(chat_queue) == 1:
+            await m.delete()
+            await m.reply(
+                f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
+                disable_web_page_preview=True,
+            )
+        else:
+            QUE = f"**🎧 SEKARANG MEMUTAR:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ DAFTAR ANTRIAN:**"
+            l = len(chat_queue)
+            for x in range(1, l):
+                hmm = chat_queue[x][0]
+                hmmm = chat_queue[x][2]
+                hmmmm = chat_queue[x][3]
+                QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`\n"
+            await m.reply(QUE, disable_web_page_preview=True)
+    else:
+        await m.reply("**❌ Tidak memutar apapun**")
+
