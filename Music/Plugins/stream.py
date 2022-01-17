b4fdc21b7dd6d4d23371729223875d98e0c2c9f0
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-01-18 03:19:27 +0700

    Update stream.py

--- Music/Plugins/stream.py
+++ Music/Plugins/stream.py
@@ -413,3 +413,47 @@ async def playlist(client, m: Message):
     else:
         await m.reply("**❌ Tidak memutar apapun**")
 
+@app.on_callback_query(filters.regex(pattern=r"VideoStream"))
+async def Videos_Stream(_, CallbackQuery):
+    if CallbackQuery.message.chat.id not in db_mem:
+        db_mem[CallbackQuery.message.chat.id] = {}
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    chat_id = CallbackQuery.message.chat.id
+    chat_title = CallbackQuery.message.chat.title
+    quality, videoid, duration, user_id = callback_request.split("|")
+    if CallbackQuery.from_user.id != int(user_id):
+        return await CallbackQuery.answer(
+            "This is not for you! Search You Own Song.", show_alert=True
+        )
+    if str(duration) == "None":
+        buttons = livestream_markup(quality, videoid, duration, user_id)
+        return await CallbackQuery.edit_message_text(
+            "**Live Stream Detected**\n\nWant to play live stream? This will stop the current playing musics(if any) and will start streaming live video.",
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
+    await CallbackQuery.message.delete()
+    title, duration_min, duration_sec, thumbnail = get_yt_info_id(videoid)
+    if duration_sec > DURATION_LIMIT:
+        return await CallbackQuery.message.reply_text(
+            f"**Duration Limit Exceeded**\n\n**Allowed Duration: **{DURATION_LIMIT_MIN} minute(s)\n**Received Duration:** {duration_min} minute(s)"
+        )
+    await CallbackQuery.answer(f"Processing:- {title[:20]}", show_alert=True)
+    theme = await check_theme(chat_id)
+    chat_title = await specialfont_to_normal(chat_title)
+    thumb = await gen_thumb(thumbnail, title, user_id, theme, chat_title)
+    nrs, ytlink = await get_m3u8(videoid)
+    if nrs == 0:
+        return await CallbackQuery.message.reply_text(
+            "Video Formats not Found.."
+        )
+    await start_video_stream(
+        CallbackQuery,
+        quality,
+        ytlink,
+        thumb,
+        title,
+        duration_min,
+        duration_sec,
+        videoid,
+    )
