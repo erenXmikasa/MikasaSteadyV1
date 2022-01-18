fba85094d72172fb3fbd81884cd479a2416c133c
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-01-18 10:07:04 +0700

    Create stream.py

--- Music/Plugins/stream.py
+++ Music/Plugins/stream.py
@@ -36,6 +36,21 @@ from Music.MusicUtilities.helpers.inline import (
 )
 
 
+def get_yt_info_query_slider(query: str, query_type: int):
+    a = VideosSearch(query, limit=10)
+    result = (a.result()).get("result")
+    title = result[query_type]["title"]
+    duration_min = result[query_type]["duration"]
+    videoid = result[query_type]["id"]
+    thumbnail = result[query_type]["thumbnails"][0]["url"].split("?")[0]
+    if str(duration_min) == "None":
+        duration_sec = 0
+    else:
+        duration_sec = int(time_to_seconds(duration_min))
+    return title, duration_min, duration_sec, thumbnail, videoid
+)
+
+
 def ytsearch(query):
     try:
         search = VideosSearch(query, limit=5).result()
