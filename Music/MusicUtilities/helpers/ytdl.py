abbfccef948b8a084b3b9da50a51b75addb32282
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-03-01 14:21:53 +0700

    [check] some checks

--- Music/MusicUtilities/helpers/ytdl.py
+++ Music/MusicUtilities/helpers/ytdl.py
@@ -2,11 +2,13 @@ from yt_dlp import YoutubeDL
 
 ytdl = YoutubeDL(
     {
-        "format": "bestaudio/best",
+        "format": "bestaudio[ext=m4a]",
         "geo-bypass": True,
+        "noprogress": True,
         "nocheckcertificate": True,
         "outtmpl": "downloads/%(id)s.%(ext)s",
+        "quite": True,
     }
 )
 
-ytdl_opts = {"format" : "bestaudio", "quiet":True}
+ytdl_opts = {"format" : "bestaudio[ext=m4a]", "quiet":True}
