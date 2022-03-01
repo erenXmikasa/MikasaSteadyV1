1527eec7b6071422baaa83dc42d1f2842b97c44c
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-03-01 16:28:47 +0700

    Update gbanmusic.py

--- Music/Plugins/gbanmusic.py
+++ Music/Plugins/gbanmusic.py
@@ -136,7 +136,7 @@ async def unban_globally(_, message):
         user = message.text.split(None, 1)[1]
         if "@" in user:
             user = user.replace("@", "")
-        user = await app.get_users(user))
+        user = await app.get_users(user)
         from_user = message.from_user
         sudoers = await get_sudoers()
         if user.id == from_user.id:
