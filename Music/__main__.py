8dac68dd38216aabec3820d7fa6710c826b4ffce
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-06-19 04:17:39 +0700

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,16 +1,23 @@
+import time
+import pytz
 import asyncio
 import time
 import uvloop
 import importlib
+from pytz import utc
 from pyrogram import Client
-from Music.config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USERS, LOG_GROUP_ID
+from Music.config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USERS, LOG_GROUP_ID, AUTO_LEAVE
 from Music import BOT_NAME, ASSNAME, app, client
 from Music.MusicUtilities.database.functions import clean_restart_stage
 from Music.MusicUtilities.database.queue import (get_active_chats, remove_active_chat)
 from Music.MusicUtilities.tgcallsrun import run
 from pytgcalls import idle
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-import time
+from Music.MusicUtilities.helpers.autoleave import leave_from_inactive_call
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+scheduler = AsyncIOScheduler()
 
 Client(
     ':Music:',
@@ -29,7 +36,7 @@ print(f"[INFO]: ASSISTANT STARTED AS {ASSNAME}!")
 async def load_start():
     restart_data = await clean_restart_stage()
     if restart_data:
-        print("[INFO]: SENDING RESTART STATUS TO PRIME SERVER")
+        print("[INFO]: SENDING RESTART STATUS TO SERVER")
         try:
             await app.edit_message_text(
                 restart_data["chat_id"],
@@ -51,9 +58,15 @@ async def load_start():
         except Exception as e:
             print("Error came while clearing db")
             pass     
-    await app.send_message(LOG_GROUP_ID, "Steady Music Telah Aktif")
-    await client.send_message(LOG_GROUP_ID, "Asisten Musik Telah Aktif")
-    print("[INFO]: STARTED BOT AND SENDING THE INFO TO STEADY SERVER SERVER")
+    await app.send_message(LOG_GROUP_ID, "Bot Started")
+    print("[INFO]: STARTED BOT AND SENDING THE INFO TO SERVER")
+    if AUTO_LEAVE:
+        print("[ INFO ] STARTING SCHEDULER")
+        scheduler.configure(timezone=pytz.utc)
+        scheduler.add_job(
+            leave_from_inactive_call, "interval", seconds=AUTO_LEAVE
+        )
+        scheduler.start()    
     
    
 loop = asyncio.get_event_loop()
@@ -63,4 +76,4 @@ run()
 idle()
 loop.close()
 
-print("[LOG] CLOSING THE STEADY MUSIC BOT")
+print("[LOG] CLOSING BOT")
